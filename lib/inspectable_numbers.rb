14aed8d791f93d1fb6fcd677c1868d4680f760b4
Andrew Radev <andrey.radev@gmail.com>
2018-08-30 10:33:23 +0300

    Extract Integer and Float extensions to core_ext.rb

--- lib/inspectable_numbers.rb
+++ lib/inspectable_numbers.rb
@@ -12,15 +12,4 @@ module InspectableNumbers
   end
 end
 
-class Integer
-  def inspect(base=10)
-    base == 10 ? InspectableNumbers.underscore_number(self) : 
-                 to_s(base)
-  end
-end
-
-class Float
-  def inspect
-    InspectableNumbers.underscore_number(self)
-  end
-end
+require "inspectable_numbers/core_ext"
